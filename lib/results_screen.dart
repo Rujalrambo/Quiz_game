f84ab9a76f764cb6730366324da184d292d33680
Rujal <rujalshrsth@gmail.com>
2025-03-18 14:46:17 +0545

    using map and loop

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,9 +1,28 @@
+import 'package:adv_basics/data/questions.dart';
 import 'package:flutter/material.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.choosenAnswers});
 
   final List<String> choosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < choosenAnswers.length; i++) {
+      // loop body
+      summary.add({
+        'question_index': i,
+        // 'question': 'Question ${i + 1}',
+        'question': questions[i].text,
+        // 'answer': choosenAnswers[i],
+        'correct_answer': questions[i].answers[0],
+        'user_answer': choosenAnswers[i],
+      });
+    }
+    return summary;
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
